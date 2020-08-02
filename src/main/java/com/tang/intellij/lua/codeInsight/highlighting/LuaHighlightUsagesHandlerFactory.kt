14a9c2837cd00e2fb6d3498e9f34acbd2112210e
tangzx <272669294@qq.com>
2020-08-02 23:03:04 +0800

    compatible with IDEA 202
    (cherry picked from commit 778edd2d90eec58ec36ae9c08c52720f907547d0)

--- src/main/java/com/tang/intellij/lua/codeInsight/highlighting/LuaHighlightUsagesHandlerFactory.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/highlighting/LuaHighlightUsagesHandlerFactory.kt
@@ -24,8 +24,6 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.Consumer
 import com.tang.intellij.lua.psi.*
-import com.tang.intellij.lua.reference.LuaIndexReference
-import com.tang.intellij.lua.reference.LuaNameReference
 
 /**
  *
@@ -38,7 +36,7 @@ class LuaHighlightUsagesHandlerFactory : HighlightUsagesHandlerFactoryBase() {
                                               psiElement: PsiElement): HighlightUsagesHandlerBase<*>? {
         when(psiElement.node.elementType) {
             LuaTypes.RETURN -> {
-            val returnStat = PsiTreeUtil.getParentOfType(psiElement, LuaReturnStat::class.java)
+                val returnStat = PsiTreeUtil.getParentOfType(psiElement, LuaReturnStat::class.java)
                 if (returnStat != null) {
                     val funcBody = PsiTreeUtil.getParentOfType(returnStat, LuaFuncBody::class.java)
                     if (funcBody != null) {
@@ -68,14 +66,6 @@ class LuaHighlightUsagesHandlerFactory : HighlightUsagesHandlerFactoryBase() {
                         override fun getTargets() = arrayListOf(psiElement)
                     }
                 }
-                else if (parent is LuaNameExpr)
-                {
-                    return LuaVarHighlightHandler(editor, psiFile, psiElement, parent)
-                }
-//                else if(parent is LuaIndexExpr) //bug有点多, GameObjectEx.FindComponent的高亮就有问题, 暂时不打开
-//                {
-//                    return LuaIndexVarHighlightHandler(editor, psiFile, psiElement, parent)
-//                }
             }
         }
         return null
@@ -89,64 +79,6 @@ private class LoopHandler(editor: Editor, psiFile: PsiFile, val psi:PsiElement, 
         loop.head?.let { addOccurrence(it) }
         loop.end?.let { addOccurrence(it) }
         addOccurrence(psi)
-    }
-
-    override fun selectTargets(list: MutableList<out PsiElement>, consumer: Consumer<in MutableList<out PsiElement>>) {}
-
-}
-
-//songtianming 点击一个变量时高亮所有class method里面的引用, 以前有这个功能?
-private class LuaVarHighlightHandler(editor: Editor, psiFile: PsiFile, val psi:PsiElement, val nameExpr: LuaNameExpr) : HighlightUsagesHandlerBase<PsiElement>(editor, psiFile) {
-    override fun getTargets(): ArrayList<PsiElement> {
-        val resolve: PsiElement? = LuaNameReference(nameExpr).resolve()
-        if (resolve != null)
-        {
-            return arrayListOf(resolve)
-        }
-        return arrayListOf()
-    }
-
-    override fun computeUsages(list: MutableList<out PsiElement>) {
-        if (list.size == 0) return
-        val resolve = list[0]
-        addOccurrence(resolve)
-        val classMethod = PsiTreeUtil.getParentOfType(psi, LuaClassMethodDef::class.java) ?: return
-        val children = PsiTreeUtil.findChildrenOfType(classMethod, LuaNameExpr::class.java)
-        children.forEach {
-            if (LuaNameReference(it).isReferenceTo(resolve)) {
-                addOccurrence(it)
-            }
-        }
-    }
-
-    override fun selectTargets(list: MutableList<out PsiElement>, consumer: Consumer<in MutableList<out PsiElement>>) {}
-
-}
-
-//songtianming 点击一个变量时高亮所有class method里面的引用, 以前有这个功能?
-private class LuaIndexVarHighlightHandler(editor: Editor, psiFile: PsiFile, val psi:PsiElement, val nameExpr: LuaIndexExpr) : HighlightUsagesHandlerBase<PsiElement>(editor, psiFile) {
-    override fun getTargets(): ArrayList<PsiElement> {
-        if (nameExpr.id != null) {
-            val resolve: PsiElement? = LuaIndexReference(nameExpr, nameExpr.id!!).resolve()
-            if (resolve != null) {
-                return arrayListOf(resolve)
-            }
-        }
-        return arrayListOf()
-    }
-
-    override fun computeUsages(list: MutableList<out PsiElement>) {
-        if (list.size == 0) return
-        val resolve = list[0]
-        addOccurrence(resolve)
-        val classMethod = PsiTreeUtil.getParentOfType(psi, LuaClassMethodDef::class.java) ?: return
-        val children = PsiTreeUtil.findChildrenOfType(classMethod, LuaIndexExpr::class.java)
-        children.forEach {
-            if (it.id != null && LuaIndexReference(it, it.id!!).isReferenceTo(resolve))
-            {
-                addOccurrence(it)
-            }
-        }
     }
 
     override fun selectTargets(list: MutableList<out PsiElement>, consumer: Consumer<in MutableList<out PsiElement>>) {}
