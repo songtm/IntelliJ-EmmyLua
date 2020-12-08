d712f6610fefe4808bac703b4aff8a5b4098beb9
songtianming <565038419@qq.com>
2020-12-08 17:27:36 +0800

    rename 强制弹提示框 self.m_xxx可能会失败

--- src/main/java/com/tang/intellij/lua/refactoring/rename/ReNameLuaIndexProcessor.kt
+++ src/main/java/com/tang/intellij/lua/refactoring/rename/ReNameLuaIndexProcessor.kt
@@ -23,40 +23,17 @@ import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.refactoring.rename.RenamePsiElementProcessor
 import com.intellij.util.MergeQuery
 import com.tang.intellij.lua.psi.LuaClassMethod
+import com.tang.intellij.lua.psi.LuaIndexExpr
 import com.tang.intellij.lua.psi.search.LuaOverridenMethodsSearch
 import com.tang.intellij.lua.psi.search.LuaOverridingMethodsSearch
 
-/**
- *
- * Created by tangzx on 2017/3/29.
- */
-class RenameLuaMethodProcessor : RenamePsiElementProcessor() {
+class ReNameLuaIndexProcessor : RenamePsiElementProcessor() {
     override fun canProcessElement(psiElement: PsiElement): Boolean {
-        return psiElement is LuaClassMethod
-    }
-
-    override fun prepareRenaming(element: PsiElement, newName: String, allRenames: MutableMap<PsiElement, String>, scope: SearchScope) {
-        val methodDef = element as LuaClassMethod
-
-        /**
-         * bug fix #167
-         * TODO: optimize this issue solution
-         * FIXME: the main reason is that `LuaNameExpr.infer` is suspended by `recursionGuard`
-         * suspended stack : rename -> ... -> LuaNameExpr.infer -> ... -> rebuild stub index -> ... -> LuaNameExpr.infer (suspended)
-         */
-        FileDocumentManager.getInstance().saveAllDocuments()
-
-        val search = MergeQuery(LuaOverridingMethodsSearch.search(methodDef), LuaOverridenMethodsSearch.search(methodDef))
-        search.forEach {
-            allRenames[it] = newName
-
-            ReferencesSearch.search(it, scope).forEach { ref ->
-                allRenames[ref.element] = newName
-            }
-        }
+        return psiElement is LuaIndexExpr
     }
 
     override fun isInplaceRenameSupported(): Boolean {
         return false;
     }
+
 }
